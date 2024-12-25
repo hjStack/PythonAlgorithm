f1e141b905a09a34651777fd4771342e78691474
hj <165659365+hjStack@users.noreply.github.com>
2024-12-25 22:44:40 +0900

    백준 1단계 입출력과 사칙연산 완료

--- day1.py
+++ day1.py
@@ -1,5 +1,5 @@
 
-# print('Hello World!').....
+# print('Hello World!')
 
 # 파이썬에서 입력받기
 # 파이썬에서 입력받을때는 input()사용
@@ -31,5 +31,40 @@
 # print(num3%num4)
 
 # 파이썬 f-string
-text=input('')
-print(f'{text}??!')
+# text=input('')
+# print(f'{text}??!')
+
+# # 백준 - 10430
+# n1,n2,n3=map(int,input().split())
+# print((n1+n2)%n3)
+# print(((n1%n3) + (n2%n3))%n3)
+# print((n1*n2)%n3)
+# print(((n1%n3) * (n2%n3))%n3)
+
+# # 18108
+# # input() 함수는 항상 문자열 형태로 입력값을 반환하기 때문에, 숫자 연산을 하려면 정수(int()) 또는 실수(float())로 변환이 필요합니다.
+# n1=int(input())
+# print(n1-543)
+
+# print('\\    /\\')
+# print(" )  ( ')")
+# print('(  /  )')
+# print(' \(__)|')
+      
+# print('|\_/|')
+# print('|q p|   /}')
+# print("( 0 )\"\"\"\\")
+# print(r'|"^"`    |')
+# print('||_/=\\__|')
+
+# 백준 11382
+# n1,n2,n3=map(int,input().split())
+# print(n1+n2+n3)
+
+# 백준 2588
+# num1=int(input())
+# num2=int(input())
+# print(num1 * (num2%10))
+# print(num1 * int((num2%100)/10))
+# print(num1 * int(num2/100))
+# print(num1*num2)
