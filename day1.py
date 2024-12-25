716fe47678d7c3e54d1f8c932af11d8292f2205e
hj <165659365+hjStack@users.noreply.github.com>
2024-12-25 16:56:44 +0900

    python basic study

---
+++ day1.py
@@ -0,0 +1,24 @@
+
+
+
+# print('Hello World!')
+
+# 파이썬에서 입력받기
+# 파이썬에서 입력받을때는 input()사용
+# 변수 여러개의 값을 입력받으려면 split()사용 
+
+# # # 더하기 
+num1,num2=map(int,input().split())
+print(num1+num2)
+
+# # # 빼기 
+num1,num2=map(int,input().split())
+print(num1-num2)
+
+# # 곱하기 
+num1,num2=map(int,input().split())
+print(num1*num2)
+
+# 나누기 
+num1,num2=map(int,input().split())
+print(num1/num2)
