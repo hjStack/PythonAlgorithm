f6662c82c915c56efd3b9e297eb5b80fedac3090
hj <165659365+hjStack@users.noreply.github.com>
2024-12-25 17:30:53 +0900

    Python Basic Study

--- day1.py
+++ day1.py
@@ -7,18 +7,27 @@
 # 파이썬에서 입력받을때는 input()사용
 # 변수 여러개의 값을 입력받으려면 split()사용 
 
-# # # 더하기 
-num1,num2=map(int,input().split())
-print(num1+num2)
+# # # # 더하기 
+# num1,num2=map(int,input().split())
+# print(num1+num2)
 
-# # # 빼기 
-num1,num2=map(int,input().split())
-print(num1-num2)
+# # # # 빼기 
+# num1,num2=map(int,input().split())
+# print(num1-num2)
 
-# # 곱하기 
-num1,num2=map(int,input().split())
-print(num1*num2)
+# # # 곱하기 
+# num1,num2=map(int,input().split())
+# print(num1*num2)
 
-# 나누기 
-num1,num2=map(int,input().split())
-print(num1/num2)
+# # 나누기 
+# num1,num2=map(int,input().split())
+# print(num1/num2)
+
+
+# 사칙 연산 총집합
+num3,num4=map(int,input().split())
+print(num3+num4)
+print(num3-num4)
+print(num3*num4)
+print('%d' %(num3/num4))
+print(num3%num4)
