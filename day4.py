3b694e0e04339bb6a0e7fc5a68637b8f6bd46436
hj <165659365+hjStack@users.noreply.github.com>
2024-12-27 22:19:49 +0900

    백준 3단계 반복문 구현

--- day4.py
+++ day4.py
@@ -32,7 +32,44 @@
 #     print('No')
     
 
-       
-        
+# 백준 - 15552
+
+# import sys
+
+# num3=int(input())
+
+# for i in range(1,num3+1):
+#     n1,n2=map(int, sys.stdin.readline().split())
+#     print(f'Case #{i}: {n1} + {n2} = {n1+n2}')
     
 
+# 백준 - 2438
+
+# num=int(input())
+    
+# for n in range(1,num+1):
+#     print(' ' * (num-n) +'*' * n)
+    
+# = 공백을 더할땐 그냥 input값에서 초기값만 빼서 하면 됨 
+
+# 백준 - 10952
+
+# while 1:
+#     n1,n2=map(int, input().split())
+    
+#     if n1 == 0 and n2 == 0:
+#         break
+#     else:
+#         print(n1+n2)
+        
+        
+# 백준 - 25314
+
+a=int(input())
+
+answer='int'
+
+for i in range(a//4):
+    print('long',end=' ')  #개행하지 않고 한 줄에 출력, 각 단어 사이에 공백 추가
+
+print(answer)
