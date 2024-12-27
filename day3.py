ee9e727951da2cbd246c68e645e3904e5c59584b
hj <165659365+hjStack@users.noreply.github.com>
2024-12-27 21:28:34 +0900

    백준 3단계 조건문 and 반복문 구현

---
+++ day3.py
@@ -0,0 +1,8 @@
+
+# 반복문 
+
+n=int(input())
+
+for i in range(n):
+    num1,num2=map(int,input().split())
+    print(num1+num2)
---
+++ day4.py
@@ -0,0 +1,38 @@
+
+# 백준 - 2739
+
+# num1=int(input())
+
+# for n in range(1,10):
+#     print(num1,'*',n,'=',num1*n)
+
+# print()
+
+# 백준 - 8393
+# num2=int(input())
+# print(sum(range(0,num2+1)))
+
+# print()
+
+# 백준 - 25304
+
+# total=int(input())
+# productNum=int(input())
+# sum1=0
+
+# for i in range(0,productNum):
+#     num1,num2=map(int,input().split())
+#     result=num1*num2
+#     sum1+=result
+    
+# if sum1 == total:
+#     print('Yes')
+    
+# else:
+#     print('No')
+    
+
+       
+        
+    
+
