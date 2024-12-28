e044d1019b6817a757093c85b8fbc3f2b3b38033
hj <165659365+hjStack@users.noreply.github.com>
2024-12-28 22:37:25 +0900

    DAY5 리스트 구현 문제

---
+++ day5.py
@@ -0,0 +1,48 @@
+
+# # 백준 - 10807
+# n=int(input())
+# a = list(map(int, input().split()))
+# find_number=int(input())
+
+# count=0
+
+# for num in a:
+#     if num == find_number:
+#         count+=1
+    
+# print(count)
+
+
+# 백준 - 10871
+
+# n,x=map(int,input().split())
+# a = list(map(int, input().split()))
+
+# for num in a:
+#     if num < x:
+#         print(num)
+
+
+# 백준 - 10818
+# n = int(input())
+
+# numbers = list(map(int, input().split()))
+
+# mi=min(numbers)
+# ma=max(numbers)
+
+# print(mi,ma)
+
+# 백준 - 2562
+# 리스트는 꼭 초기화 하기 !!
+# numbers=[]
+
+# for i in range(0,9):
+#     n=int(input())
+#     numbers.append(n)
+
+# ma=max(numbers)
+# print(ma)
+# print(numbers.index(ma)+1)  # 특정 숫자 인덱스 찾기
+
+
