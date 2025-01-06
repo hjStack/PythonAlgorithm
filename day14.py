2bb0579e5b38040984773f23125497fd1678b379
혜준 <minnie1498@naver.com>
2025-01-06 23:57:42 +0900

    약수,배수와 소수

---
+++ day14.py
@@ -0,0 +1,25 @@
+
+# 백준 2745
+
+# n,b=input().split()
+# print(int(n,int(b)))
+# # int(x, radix)
+# # radix 진수로 표현된 문자열 x를 10진수로 변환하여 리턴한다. ⭐️
+
+# 백준 - 배수와 약수 구하기(5086) 
+# while(True):
+#     a,b=map(int,input().split())
+    
+#     if a==0 and b==0:
+#         break
+    
+#     elif a%b==0:
+#         print('multiple')
+        
+#     elif b%a==0:
+#         print('factor')
+           
+#     else:
+#         print('neither')
+            
+
