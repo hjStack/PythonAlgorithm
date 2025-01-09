a14c58e83db869e9f6a9dd128d4befea764c960c
혜준 <minnie1498@naver.com>
2025-01-09 23:26:02 +0900

    글자 세로로 출력하기 고민해보기

---
+++ day17.py
@@ -0,0 +1,30 @@
+
+# 백준 - 10798 ... 
+
+# for i in range(5):
+#     n=input()
+    
+# result=[]
+# new = ''
+    
+# for i in range(5):
+#     for j in range(5):
+#         result.append(n)
+        
+# 백준 - 27323
+
+# n=int(input().strip())
+# m=int(input().strip())
+
+# area=n*m
+# print(area)
+
+# 백준 - 한수의 현재위치와 오른쪽 위 꼭짓점 사이까지의 최소 거리 - 1085
+# x, y, w, h = map(int,input().split())
+# a=w-x
+# b=h-y
+
+# # 문제 예시로 보면 (4,1)
+# print(min(x,y,a,b))
+
+# 백준 - 3009
