32b6b40c93ed88da467bd11ba864fc4ee307b640
hjStack <minnie1498@naver.com>
2025-01-17 20:38:08 +0900

    소수구하기

--- day25.py
+++ day25.py
@@ -24,6 +24,30 @@
 # 어떠한 수에 의해 나누어 떨어지는지 확인하는 것
 # 나누어 떨어지면 해당 수는 소수이고, 다른 수에 의해 나누어 떨어진다면 그 수는 소수가 아님
 
+# def isPrime_number(n):
+#     if n<=1:
+#         return False
+#     else:
+#         for i in range(2,n-1):
+#             if n%i == 0:   # 소수가 아님
+#                 return False
+#     return True
+
+# n=int(input())
+# m=list(map(int,input().split()))
+# count=0
+
+# for i in range(n):
+#      if isPrime_number(m[i]):
+#         count+=1
+        
+# print(count)
+
+# print("======================================================");
+
+# 소수의 합과 최솟값 
+# 처음만나는 값이 최솟값이다 
+
 def isPrime_number(n):
     if n<=1:
         return False
@@ -34,12 +58,23 @@ def isPrime_number(n):
     return True
 
 n=int(input())
-m=list(map(int,input().split()))
+m=int(input())
 count=0
+sum=0
+min1=0
 
-for i in range(n):
-     if isPrime_number(m[i]):
-        count+=1
-        
-print(count)
+# n=60이고 m=100인경우 
 
+for i in range(n,m+1):
+    if isPrime_number(i):
+        sum = sum +i
+        if min1==0: # 가장 처음 발견된 수가 가장 작은 값이므로 대입
+            min1=i 
+            
+if sum == 0:
+    print(-1)
+else:
+    print(sum)
+    print(min1)
+
+ 
